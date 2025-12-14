1b69834a6ebefffae292a62c137fab1f53dbeb55
CHOIHYUKMIN <hyukchm@gmail.com>
2025-12-14 10:25:31 +0900

    Bump version to 2.3.2

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
     APP_DESCRIPTION: 'AI가 당신의 얼굴로 실물 나이를 추측하고, 10가지 질문으로 마음의 나이를 알려드려요',
 
     // 버전 관리 (한 곳에서 관리)
-    VERSION: '2.3.1',
+    VERSION: '2.3.2',
     VERSION_NAME: 'Christmas Edition 🎄'
 };
 
